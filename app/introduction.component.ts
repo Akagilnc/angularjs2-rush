cc19cfbcf9e740e03f84bbcb34c969ddddc465a1
DeronLee <akagilnc@gmail.com>
2016-05-16 00:44:17 +0800

    change font color to white and button color to green on introduction page

--- app/introduction.component.ts
+++ app/introduction.component.ts
@@ -4,8 +4,8 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 @Component({
   selector: 'introduction',
   template: `
-  <p>Welcome to our app</p>
-  <a class='btn btn-primary' [routerLink]='["PlayComponent"]'>Start game</a>
+  <p style="color: white">Welcome to our app</p>
+  <a class='btn btn-success' [routerLink]='["PlayComponent"]'>Start game</a>
   `,
   directives: [ROUTER_DIRECTIVES]
 })
